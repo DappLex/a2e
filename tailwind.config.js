48d4320bd755203aaab714221dca0defda72e38e
DappLex <gospelalex01@gmail.com>
2023-06-26 11:03:18 +0100

    My first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+    corePlugins: {
+        // Enable the background-image utility
+        backgroundImage: true,
+    },
+};
